ccb88a04b39360c1b6af20155f1c6c15b6967271
samuel <samuel@samuel-notebook>
2012-08-10 17:01:53 -0300

    Mudei denovo

---
+++ src/java/Bean/teste.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author samuel
+ */
+@ManagedBean
+@RequestScoped
+public class teste {
+
+    /**
+     * Creates a new instance of teste
+     */
+    public teste() {
+    }
+}
