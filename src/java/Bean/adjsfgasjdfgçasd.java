6e068c8cd93d2aeeb8c478eea858a8ff762ac15c
samuel <samuel@samuel-notebook>
2012-08-15 15:30:56 -0300

    Adicionei nome feio

---
+++ src/java/Bean/adjsfgasjdfgçasd.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bean;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author samuel
+ */
+@Stateless
+public class adjsfgasjdfgçasd {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+}
